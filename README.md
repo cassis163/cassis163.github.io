3c14af140c2ff1703da84d2d1d18a478470751e6
Casper Aangeenbrug <casper.aangeenbrug@omoda.nl>
2023-10-13 16:25:17 +0200

    Render HTML in blog pages

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Welcome to my blog
+Welcome to my blog that is built with an overly complicated tech stack. I did this on purpose though, because I wanted to learn Golang and apply it at least one project.
 
 ## Development
 Build (and watch) template files:
--- src/handlers/blog_handler.go
+++ src/handlers/blog_handler.go
@@ -1,8 +1,11 @@
 package handlers
 
 import (
+	"context"
+	"io"
 	"net/http"
 
+	"github.com/a-h/templ"
 	"github.com/gin-gonic/gin"
 
 	components "github.com/cassis163/personal-site/components"
@@ -21,8 +24,17 @@ func NewBlogHandler() *BlogHandler {
 
 func (h *BlogHandler) Handle(c *gin.Engine) {
 	for _, blogPost := range h.BlogService.BlogPosts {
+		html := h.BlogService.GenerateMdFromBlogPost(blogPost)
+		component := Unsafe(string(html))
 		c.GET("/blog/"+blogPost.FileName, func(c *gin.Context) {
-			c.HTML(http.StatusOK, "blog_post.html", components.BlogPostPage())
+			c.HTML(http.StatusOK, "blog_post.html", components.BlogPostPage(blogPost.Title, component))
 		})
 	}
+}
+
+func Unsafe(html string) templ.Component {
+	return templ.ComponentFunc(func(ctx context.Context, w io.Writer) (err error) {
+		_, err = io.WriteString(w, html)
+		return
+	})
 }
